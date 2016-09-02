6d6a68a00af06652de9e2ec38a6a337e01b4cd18
Michael Gibson <michael.gibson79@gmail.com>
2016-09-01 19:17:28 -0600

    initial checkin

--- README.md
+++ README.md
@@ -1,2 +1,136 @@
-# heka-config-manager
-Configuration Management for Heka....through Heka
+# Heka Configuration Management
+======================
+
+Configuration Management for [Heka](http://hekad.readthedocs.io/en/latest/)....using [Heka](http://hekad.readthedocs.io/en/latest/)
+
+Occasionally there are circumstances where it may not always be possible to leverage the existing configuration management service(assuming there is one) for the environment in which you have deployed Heka.
+In this scenario it may be useful to be able to deliver and deploy new configurations using the Heka daemon itself.
+
+One of Heka's Input types is the
+[ProcessDirectoryInput](https://hekad.readthedocs.io/en/latest/config/inputs/processdir.html).
+
+That input scans a specified directory at a set interval for another Heka input type([ProcessInput](https://hekad.readthedocs.io/en/latest/config/inputs/process.html#config-process-input)) and dynamically loads or unloads them as they appear.
+
+Expanding on this model we can introduce similar new input types(i.e. LogstreamerDirectoryInput) to monitor locations for their own updates.
+
+All we need is a way to manage the config files each DirectoryInput loads.
+
+CMFilter
+===========
+
+This filter provides the following functionality:
+- Accepts new configuration updates using the contents of a Heka message payload.
+- Returns contents of existing config files
+- Removes specified config files
+
+It will expect the incoming Heka message to have one or more of the following Fields set:
+- Fields[Action](string, required)
+	Value must be one of:
+	-	"add": Add new or update existing config file.
+		Fields["Overwrite"]: (string, optional) - Indicates whether to overwrite an existing config file or fail. Duplication is determined based on name of plugin. Defaults to "false"
+	-	"delete": Removes config files indicated by "Filename" Field(required)
+		-	Fields["Filename"]: Name of file to be removed. Automatically removes empty ticker directory if under "process_dir"
+	- "return": Recursively returns all configurations under "include_paths"
+
+Config:
+
+- cm_tag(string, optional):
+		Optional tagging for identifying output from filter.
+		This setting creates a new Heka Field called "CMTag" and is given the value of this option. Defaults to "CM"
+
+- include_types([]string, optional):
+		PluginTypes to target. Defaults to {"ProcessInput", "LogstreamerInput"}
+
+- exclude_paths([]string, optional):
+		Explicitly exclude files or directories from filter "Actions".
+		For config paths that absolutely must not be touched.
+		Overrides include_paths.
+
+- process_dir(string, optional):
+		The location of [ProcessInput](https://hekad.readthedocs.io/en/latest/config/inputs/process.html#config-process-input) configurations that to be processed by a [ProcessDirectoryInput](https://hekad.readthedocs.io/en/latest/config/inputs/processdir.html)
+		Defaults to "$share_dir/processes.d"
+
+- logstreamer_dir(string, optional):
+		The location of [LogstreamerInput](https://hekad.readthedocs.io/en/latest/config/inputs/logstreamer.html) configurations that to be processed by a [LogstreamerDirectoryInput](https://github.com/michaelgibson/heka-logstreamer-directory-input)
+		Defaults to "$share_dir/processes.d"
+
+Example Filter Configuration:
+
+	[cm_filter]
+	type = "CMFilter"
+	message_matcher = "(Fields[Action] == 'return' || Fields[Action] == 'add' || Fields[Action] == 'delete') && Fields[CMTag] == NIL"
+	cm_tag = "CM"
+	include_paths = ["/usr/share/heka/processes.d", "/usr/share/heka/logstreamers.d"]
+	exclude_paths = ["/data/heka/protoin/"]
+	include_types = ["ProcessInput", "LogstreamerInput"]
+	process_dir = "/usr/share/heka/processes.d"
+	logstreamer_dir = "/usr/share/heka/logstreamers.d"
+	use_buffering = true
+
+
+Example "add" Config Message with syslog LogstreamerInput:
+
+	:Timestamp: 2016-09-01 22:36:43.641743638 +0000 UTC
+	:Type: LogstreamerInput
+	:Hostname: foo
+	:Pid: 28970
+	:Uuid: 07d2911d-e6ab-445d-ad4a-676ecae5597b
+	:Logger: add_config
+	:Payload: [syslog]
+	type="LogstreamerInput"
+	log_directory = "/var/log"
+	file_match = 'auth\.log'
+	decoder = "RsyslogDecoder"
+	journal_directory = "/var/cache/hekad/logstreamer_dir_inputs"
+
+	:EnvVersion:
+	:Severity: 7
+	:Fields:
+	    | name:"Action" type:string value:"add"
+	    | name:"Overwrite" type:string value:"true" representation:"bool"
+
+
+Example "delete" Config Message:
+
+	:Timestamp: 2016-09-01 22:48:58.753701221 +0000 UTC
+	:Type: LogstreamerInput
+	:Hostname: foo
+	:Pid: 29322
+	:Uuid: 113d0841-2dd3-4546-9c04-f4aa10a17362
+	:Logger: delete_config
+	:Payload:
+
+	:EnvVersion:
+	:Severity: 7
+	:Fields:
+	    | name:"Action" type:string value:"delete"
+	    | name:"Filename" type:string value:"/usr/share/heka/logstreamers.d/heka79a52d9b9c5e68595596cc03d984e83a.toml"
+
+Example "return" Config Message
+
+	:Timestamp: 2016-09-01 22:52:03.581118015 +0000 UTC
+	:Type: LogstreamerInput
+	:Hostname: foo
+	:Pid: 29420
+	:Uuid: 705f52d2-0589-41b9-b2b8-42139c270b00
+	:Logger: return_config
+	:Payload:
+
+	:EnvVersion:
+	:Severity: 7
+	:Fields:
+	    | name:"Action" type:string value:"return"
+
+To Build
+========
+
+See [Building *hekad* with External Plugins](http://hekad.readthedocs.org/en/latest/installing.html#build-include-externals)
+for compiling in plugins.
+
+Edit cmake/plugin_loader.cmake file and add
+
+    add_external_plugin(git https://github.com/michaelgibson/heka-config-manager master)
+
+Build Heka:
+	. ./build.sh
+
---
+++ config_manager_filter.go
@@ -0,0 +1,616 @@
+package config_management
+
+
+import (
+				"bytes"
+				"crypto/rand"
+				"encoding/hex"
+        "fmt"
+				"io"
+				"io/ioutil"
+				"os"
+				"path/filepath"
+				"strconv"
+				"strings"
+				"sync"
+				"github.com/bbangert/toml"
+        "github.com/mozilla-services/heka/message"
+        . "github.com/mozilla-services/heka/pipeline"
+        "github.com/pborman/uuid"
+
+)
+
+const (
+	PROCESSINPUT_TICKER = 15
+)
+
+type CMBatch struct {
+	queueCursor string
+	count       int64
+	batch       []byte
+	confInfo		ConfigFileInfo
+}
+
+type ConfigFileInfo struct {
+	confName			string
+	confType			string
+	confCategory	string
+	fileName		  string
+	overwrite			bool
+	status				string
+	ticker				int
+}
+
+type CMFilter struct {
+  *CMFilterConfig
+	count            int64
+	backChan         chan []byte
+	recvChan         chan MsgPack
+	batchChan        chan CMBatch // Chan to pass completed batches
+	outBatch         []byte
+	queueCursor      string
+	conf             *CMFilterConfig
+	fr               FilterRunner
+	outputBlock      *RetryHelper
+	reportLock       sync.Mutex
+	pConfig          *PipelineConfig
+	stopChan         chan bool
+	msgLoopCount 		 uint
+	msg 						 *message.Message
+	confInfo				 ConfigFileInfo
+	includePaths		 []string
+	shareDir				 string
+}
+
+type CMFilterConfig struct {
+	CMTag        string   `toml:"cm_tag"`
+	ExcludePaths []string `toml:"exclude_paths"`
+	IncludeTypes []string `toml:"include_types"`
+
+
+	//Used for ProcessDirectoryInputs
+	ProcessDir		 string 	`toml:"process_dir"`
+	LogstreamerDir string		`toml:"logstreamer_dir"`
+
+	UseBuffering bool `toml:"use_buffering"`
+}
+
+type MsgPack struct {
+	bytes       []byte
+	Message			*message.Message
+	MsgLoopCount uint
+	queueCursor string
+}
+
+func (f *CMFilter) ConfigStruct() interface{} {
+  return &CMFilterConfig{
+		CMTag: "CM",
+		IncludeTypes: []string{"ProcessInput", "LogstreamerInput"},
+		UseBuffering:          true,
+  }
+}
+
+func (f *CMFilter) Init(config interface{}) (err error) {
+  f.conf = config.(*CMFilterConfig)
+
+	f.batchChan = make(chan CMBatch)
+	f.backChan = make(chan []byte, 2)
+	f.recvChan = make(chan MsgPack, 100)
+
+  return
+}
+
+func (f *CMFilter) Prepare(fr FilterRunner, h PluginHelper) error {
+	f.fr = fr
+	f.stopChan = fr.StopChan()
+	f.confInfo.ticker = PROCESSINPUT_TICKER
+	f.pConfig = h.PipelineConfig()
+	f.shareDir = f.pConfig.Globals.ShareDir
+
+	if f.conf.ProcessDir == "" {
+		f.conf.ProcessDir = filepath.Join(f.shareDir, "processes.d")
+	}
+
+	if f.conf.LogstreamerDir == "" {
+		f.conf.LogstreamerDir = filepath.Join(f.shareDir, "logstreamers.d")
+	}
+
+	f.includePaths = append(f.includePaths, f.conf.ProcessDir)
+	f.includePaths = append(f.includePaths, f.conf.LogstreamerDir)
+
+	var err error
+	f.outputBlock, err = NewRetryHelper(RetryOptions{
+		MaxDelay:   "5s",
+		MaxRetries: -1,
+	})
+	if err != nil {
+		return fmt.Errorf("can't create retry helper: %s", err.Error())
+	}
+
+	f.outBatch = make([]byte, 0, 10000)
+	go f.committer(h)
+	go f.batchSender(h)
+	return nil
+
+}
+
+func (f *CMFilter) ProcessMessage(pack *PipelinePack) error {
+	var (
+		outBytes []byte
+	)
+
+
+	outBytes = []byte(pack.Message.GetPayload())
+
+	if outBytes != nil {
+		f.recvChan <- MsgPack{bytes: outBytes, queueCursor: pack.QueueCursor, Message: pack.Message, MsgLoopCount: pack.MsgLoopCount}
+	}
+
+	return nil
+}
+
+func (f *CMFilter) batchSender(h PluginHelper) {
+	ok := true
+	outBytes := make([]byte, 0, 10000)
+
+	for ok {
+		select {
+		case <-f.stopChan:
+			ok = false
+			continue
+		case pack := <-f.recvChan:
+
+			f.msgLoopCount = pack.MsgLoopCount
+			action, _ := pack.Message.GetFieldValue("Action")
+			f.confInfo = ConfigFileInfo{
+					confName:			"",
+					confType:			"",
+					confCategory:	"",
+					fileName:		  "",
+					overwrite:		false,
+					status:				"OK",
+					ticker:				PROCESSINPUT_TICKER,
+			}
+
+			switch action {
+				case "add":
+					payload := pack.Message.GetPayload()
+					if value, exists := pack.Message.GetFieldValue("Overwrite"); exists {
+						field := pack.Message.FindFirstField("Overwrite")
+						valueType := field.GetValueType()
+						if valueType == message.Field_STRING {
+							f.confInfo.overwrite, _ = strconv.ParseBool(value.(string))
+						}
+					}
+
+					tomlSection, _  := f.getDecodedToml(payload)
+
+					CONFIG:
+					for name, conf := range tomlSection {
+						f.confInfo.confName, f.confInfo.confType, f.confInfo.confCategory, f.confInfo.ticker = f.getConfigFileInfo(name, conf)
+						for _, confType := range f.conf.IncludeTypes {
+							if f.confInfo.confType == confType {
+								outBytes = []byte(f.addConfig(payload, f.confInfo))
+								continue CONFIG
+							}
+						}
+					}
+
+				case "delete":
+					if value, exists := pack.Message.GetFieldValue("Filename"); exists {
+						field := pack.Message.FindFirstField("Filename")
+						valueType := field.GetValueType()
+						if valueType == message.Field_STRING {
+								outBytes = []byte(f.removeConfig(value.(string)))
+						}
+					} else {
+						f.confInfo.status = "ERROR"
+						outBytes = []byte("You must specify a 'Filename' Field when deleting.")
+					}
+
+				case "return":
+					f.queueCursor = pack.queueCursor
+					for _, configPath := range f.includePaths {
+						err := f.sendConfigContents(configPath)
+						if err != nil {
+							fmt.Println(err)
+							f.pConfig.Globals.ShutDown(1)
+							break
+						}
+					}
+				default:
+					outBytes = []byte("ERROR Applying Configuration: You must supply an action(add, remove, return)")
+				}
+				f.outBatch = append(f.outBatch, outBytes...)
+				f.queueCursor = pack.queueCursor
+				f.count++
+				if len(f.outBatch) > 0 {
+					f.sendBatch()
+				}
+			}
+
+	}
+}
+
+func (f *CMFilter) sendBatch() {
+	b := CMBatch{
+		queueCursor: f.queueCursor,
+		count:       f.count,
+		batch:       f.outBatch,
+		confInfo:    f.confInfo,
+	}
+	f.count = 0
+	select {
+	case <-f.stopChan:
+		return
+	case f.batchChan <- b:
+	}
+	select {
+	case <-f.stopChan:
+	case f.outBatch = <-f.backChan:
+	}
+}
+
+func (f *CMFilter) committer(h PluginHelper) {
+	f.backChan <- make([]byte, 0, 10000)
+
+	var (
+	    b CMBatch
+      tag string
+	)
+	tag = f.conf.CMTag
+
+	ok := true
+	for ok {
+		select {
+		case <-f.stopChan:
+			ok = false
+			continue
+		case b, ok = <-f.batchChan:
+			if !ok {
+				continue
+			}
+		}
+
+		pack, _ := h.PipelinePack(f.msgLoopCount)
+
+		fileNameField, _ := message.NewField("FileName", b.confInfo.fileName, "")
+		pack.Message.AddField(fileNameField)
+
+		confNameField, _ := message.NewField("ConfName", b.confInfo.confName, "")
+		pack.Message.AddField(confNameField)
+
+		confTypeField, _ := message.NewField("ConfType", b.confInfo.confType, "")
+		pack.Message.AddField(confTypeField)
+
+		confCategoryField, _ := message.NewField("ConfCategory", b.confInfo.confCategory, "")
+		pack.Message.AddField(confCategoryField)
+
+		if b.confInfo.confType == "ProcessInput" {
+			confTickerField, _ := message.NewField("Ticker", strconv.Itoa(b.confInfo.ticker), "")
+			pack.Message.AddField(confTickerField)
+		}
+
+		statusField, _ := message.NewField("Status", b.confInfo.status, "")
+		pack.Message.AddField(statusField)
+
+		tagField, _ := message.NewField("CMTag", tag, "")
+    pack.Message.AddField(tagField)
+    pack.Message.SetUuid(uuid.NewRandom())
+    pack.Message.SetPayload(string(b.batch))
+
+		f.fr.Inject(pack)
+		f.fr.UpdateCursor(b.queueCursor)
+
+		b.batch = b.batch[:0]
+		f.backChan <- b.batch
+	}
+}
+
+//BEGIN "add" Config methods
+func (f *CMFilter) addConfig(payload string, cfi ConfigFileInfo) (result string) {
+	var (
+		buffer bytes.Buffer
+		dup	= false
+		dir string
+		existingCFI ConfigFileInfo
+	)
+
+ 	switch cfi.confType {
+	case "ProcessInput":
+		dir = filepath.Clean(f.conf.ProcessDir) + "/" + fmt.Sprint(cfi.ticker)
+	case "LogstreamerInput":
+		dir = filepath.Clean(f.conf.LogstreamerDir)
+	default:
+		f.confInfo.status = "ERROR"
+		return fmt.Sprintf("Unable to determine configuration directory.")
+	}
+
+	if _, err := os.Stat(dir); os.IsNotExist(err) {
+		os.Mkdir(dir, 0755)
+	}
+
+	randBytes := make([]byte, 16)
+	rand.Read(randBytes)
+	fName := "heka"+hex.EncodeToString(randBytes)+".toml"
+	confFile := filepath.Join(dir, fName)
+
+	//check for duplicate entries
+	files, _ := ioutil.ReadDir(dir)
+	for _, file := range files {
+		existingFName := filepath.Join(dir, file.Name())
+		fileContents, _ := ReplaceEnvsFile(existingFName)
+		fileTomlConfig, _  := f.getDecodedToml(fileContents)
+		for name, conf := range fileTomlConfig {
+			existingCFI.confName, existingCFI.confType, existingCFI.confCategory, existingCFI.ticker = f.getConfigFileInfo(name, conf)
+
+			//Check if there is already a config with this name at the same ticker interval.
+			if cfi.confName == existingCFI.confName && cfi.ticker == existingCFI.ticker {
+				dup = true
+			}
+		}
+
+		if payload == fileContents || dup {
+			f.confInfo.fileName = existingFName
+			if !cfi.overwrite {
+				f.confInfo.status = "ERROR"
+				return fmt.Sprintf("Duplicate config already exists for this interval and overwrite is disabled.\nNot adding configuration.\nFilename: %s\n%s", existingFName, fileContents)
+			} else {
+				confFile = existingFName
+				buffer.WriteString(fmt.Sprintf("Overwriting existing configuration.\nFilename: %s\n%s\n", existingFName, fileContents))
+			}
+		}
+	}
+	err := ioutil.WriteFile(confFile, []byte(payload), 0644)
+
+	if err != nil {
+		f.confInfo.status = "ERROR"
+		return fmt.Sprintf("Failed due to error: %s", err.Error())
+	}
+	f.confInfo.fileName=confFile
+	buffer.WriteString("Successfully updated configuation!")
+	return buffer.String()
+}
+
+func (f *CMFilter) getDecodedToml(contents string) (configFile ConfigFile, err error) {
+	//attempt make toml primitive from string
+	if _, err = toml.Decode(contents, &configFile); err != nil {
+		return configFile, fmt.Errorf("Error decoding config file: %s", err)
+	}
+	return configFile, err
+}
+//END "add" Config methods
+
+//BEGIN "remove" Config methods
+func (f *CMFilter) removeConfig(fName string) (result string) {
+	f.confInfo.fileName = fName
+	fi, err := os.Stat(fName)
+
+	if err != nil {
+		f.confInfo.status = "ERROR"
+		result = fmt.Sprintf("can't stat file: " + err.Error())
+		return result
+	}
+
+	//I don't always delete directories, but when I do, I prefer that they are only empty sub-directories of the ProcessDir
+	if fi.IsDir() && f.confInfo.confType == "ProcessDirectoryInput" {
+		err := f.checkProcessTickerDir(fName)
+		if err != nil {
+		//Stay employed my friends...
+			f.confInfo.status = "ERROR"
+			result = fmt.Sprintf("You cannot delete this directory: %s", fName)
+			return result
+		}
+	}
+
+	parentDir := filepath.Dir(fName)
+
+	_, err = f.checkPath(fName)
+
+	if err != nil {
+		f.confInfo.status = "ERROR"
+		result = fmt.Sprintf(err.Error())
+		return result
+	}
+
+	err = os.Remove(fName)
+
+	if err != nil {
+		f.confInfo.status = "ERROR"
+		result = fmt.Sprintf(err.Error())
+		return result
+	}
+
+
+	//check if parent dir is now empty
+	empty, err := IsEmpty(parentDir)
+	//if so is it removable too?
+	if empty && f.confInfo.confType == "ProcessDirectoryInput" {
+		err = f.checkProcessTickerDir(parentDir)
+		if err == nil {
+				err = os.Remove(parentDir)
+				result = fmt.Sprintf("Successfully removed file: %s as well as empty process ticker directory: %s", fName, parentDir)
+				return result
+		} else {
+				f.confInfo.status = "ERROR"
+				result = fmt.Sprintf("Successfully removed file: %s but failed to remove process ticker directory: %s", fName, parentDir)
+				return result
+		}
+	}
+
+	result = fmt.Sprintf("Successfully deleted file: %s", fName)
+	return result
+}
+
+func (f *CMFilter) checkPath(path string) (ok bool, err error) {
+	ok = false
+	for _, incPath := range f.includePaths {
+		for _, exPath := range f.conf.ExcludePaths {
+			if strings.HasPrefix(path, exPath) {
+				err = fmt.Errorf("'%s' is under exclude_paths '%s'. Skipping.", path, exPath)
+				return false, err
+			}
+		}
+		if strings.HasPrefix(path, incPath) {
+			ok = true
+		}
+	}
+	return ok, nil
+}
+
+func (f *CMFilter) checkProcessTickerDir(fName string) (err error) {
+	empty, err := IsEmpty(fName)
+	if !strings.HasPrefix(fName, f.conf.ProcessDir) || filepath.Clean(fName) == filepath.Clean(f.conf.ProcessDir) || !empty || err != nil {
+		err = fmt.Errorf("You cannot delete this directory: %s", fName)
+		return err
+	}
+	return err
+}
+
+func IsEmpty(name string) (bool, error) {
+    f, err := os.Open(name)
+    if err != nil {
+        return false, err
+    }
+    defer f.Close()
+
+    _, err = f.Readdirnames(1) // Or f.Readdir(1)
+    if err == io.EOF {
+        return true, nil
+    }
+    return false, err // Either not empty or error, suits both cases
+}
+//END "remove" Config methods
+
+//BEGIN "return" Action methods
+func (f *CMFilter) getConfigFileInfo(name string, configFile toml.Primitive) (configName string, configType string, configCategory string, ticker int) {
+	//Get identifiers from config section
+	//using ProcessInput's "current" default of 15 seconds for ticker
+	ticker = f.confInfo.ticker
+	pipeConfig := NewPipelineConfig(nil)
+	maker, _ := NewPluginMaker(name, pipeConfig, configFile)
+	if maker.Type() != "" {
+		if maker.Category() == "Input" {
+			mutMaker := maker.(MutableMaker)
+			commonConfig, _ := mutMaker.OrigPrepCommonTypedConfig()
+			commonInput := commonConfig.(CommonInputConfig)
+			ticker = int(commonInput.Ticker)
+		}
+		return maker.Name(), maker.Type(), maker.Category(), ticker
+	}
+	return "", "", "", ticker
+}
+
+func (f *CMFilter) sendConfigContents(configPath string) (err error) {
+	var (
+		confString string
+		ok				 bool
+		pluginType string
+    section  	 toml.Primitive
+	 )
+
+	outBytes := make([]byte, 0, 10000)
+
+	p, err := os.Open(configPath)
+	if err != nil {
+		return fmt.Errorf("error opening file: " + err.Error())
+	}
+
+	fi, err := p.Stat()
+	if err != nil {
+		return fmt.Errorf("can't stat file: " + err.Error())
+	}
+
+		//If path is directory, recursively walk it to find configs
+	if fi.IsDir() {
+		files := []string{}
+		filepath.Walk(configPath, func(path string, f os.FileInfo, err error) error {
+			files = append(files, path)
+			return nil
+		})
+
+		FILES:
+			for _, fName := range files {
+				if !strings.HasSuffix(fName, ".toml") {
+					// Skip non *.toml files in a config dir.
+					continue
+				}
+
+				for _, excludePath := range f.conf.ExcludePaths {
+					if strings.HasPrefix(fName, excludePath) {
+						fmt.Printf("'%s' is under excludePath '%s'. Skipping.", fName, excludePath)
+						continue FILES
+					}
+				}
+
+				unparsedConfig := make(map[string]toml.Primitive)
+				confString, _ = ReplaceEnvsFile(fName)
+				_, err = toml.Decode(confString, &unparsedConfig)
+
+				for _, pluginType = range f.conf.IncludeTypes {
+					section, ok = unparsedConfig[pluginType]
+					if ok {
+							break
+					}
+				}
+				f.confInfo.confName, f.confInfo.confType, f.confInfo.confCategory, f.confInfo.ticker = f.getConfigFileInfo(pluginType, section)
+				f.confInfo.fileName = fName
+				outBytes = []byte(confString)
+				f.outBatch = append(f.outBatch, outBytes...)
+				f.count++
+				f.confInfo.status = "OK"
+
+				if len(f.outBatch) > 0 {
+					f.sendBatch()
+				}
+			}
+	} else {
+		//Path is absolute location
+		if !strings.HasSuffix(configPath, ".toml") {
+			// Skip non *.toml files in a config dir.
+			return nil
+		}
+		//Skip if path is excluded from config
+		for _, excludePath := range f.conf.ExcludePaths {
+			if configPath == excludePath || filepath.Clean(configPath) == filepath.Clean(excludePath) {
+				return nil
+			}
+		}
+
+		unparsedConfig := make(map[string]toml.Primitive)
+		confString, _ = ReplaceEnvsFile(configPath)
+		_, err = toml.Decode(confString, &unparsedConfig)
+
+		for _, pluginType = range f.conf.IncludeTypes {
+			section, ok = unparsedConfig[pluginType]
+			if ok {
+				break
+			}
+		}
+		f.confInfo.confName, f.confInfo.confType, f.confInfo.confCategory, f.confInfo.ticker = f.getConfigFileInfo(pluginType, section)
+		f.confInfo.fileName = configPath
+		f.confInfo.status = "OK"
+
+		outBytes = []byte(confString)
+		f.outBatch = append(f.outBatch, outBytes...)
+		f.count++
+		if len(f.outBatch) > 0 {
+			f.sendBatch()
+		}
+	}
+	return nil
+}
+//END "return" Action methods
+
+
+func (f *CMFilter) CleanUp() {
+
+}
+
+
+
+func init() {
+    RegisterPlugin("CMFilter", func() interface{} {
+        return new(CMFilter)
+    })
+}
